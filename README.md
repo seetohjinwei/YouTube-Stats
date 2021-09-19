0d03073bc057bdf3052404c5af9540e34fcd617c
Jin Wei <seetohjinwei@gmail.com>
2021-09-19 21:02:29 +0800

    Added mass parser

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # YouTube Stats
 Built by See Toh Jin Wei
 
-Polls links from CSV and updates into same CSV.
+Grabs links from CSV (or plaintext) and updates information into same CSV.
 
 Grabs the following data:
 1. Title
@@ -21,7 +21,13 @@ Requirements:
 Instructions to use:
 1. Download `main.py` and `videos.csv`
 2. Add your API key onto line 53 (near the end) of `main.py`
-3. Paste a YouTube video URL on a separate line each
+3. Paste a YouTube video URL (only the ID portion) on a separate line each
 4. [Optional] Add a name to a video by adding `,NAME` to the end of the url
+5. Run `main.py` with `videos.csv` in the same folder.
 
-Example: `https://www.youtube.com/watch?v=gjyEcSim4js,Cool Video`
+Example: `gjyEcSim4js,Cool Music`
+
+To use parser:
+1. Copy paste as many YouTube links as needed in the `data` file in plaintext. `youtu.be` links **not** supported.
+2. Can have irrelevant text inside. Only YouTube links will be detected.
+3. Run `parse.py` with `main.py` and `videos.csv` in the same folder.
--- main.py
+++ main.py
@@ -17,9 +17,13 @@ class YouTube:
         self.build = build("youtube", "v3", developerKey=api_key)
     
     def get_data(self, videoID: str):
-        video = self.build.videos().list(part="statistics",id=videoID).execute()
-        title: str = self.build.videos().list(part="snippet", id=videoID).execute()["items"][0]["snippet"]["title"]
-        stats = video['items'][0]['statistics']
+        try:
+            video = self.build.videos().list(part="statistics",id=videoID).execute()
+            title: str = self.build.videos().list(part="snippet", id=videoID).execute()["items"][0]["snippet"]["title"]
+            stats = video['items'][0]['statistics']
+        except IndexError:
+            print(f"Invalid link skipped: {videoID}")
+            return None
         return Video(
             videoID,
             title,
@@ -32,13 +36,10 @@ class YouTube:
 def main(API_KEY: str):
     youtube: YouTube = YouTube(api_key=API_KEY)
     df: pd.DataFrame = pd.read_csv("videos.csv", index_col=False, dtype=str)
-    pattern: Pattern = compile(r'[a-zA-Z0-9-_]{11,12}')
     for index, row in df.iterrows():
-        ID: list[str] = pattern.findall(row["URL"])
-        if len(ID) != 1:
-            print(f"Invalid URL on line {index + 1}. Skipped!")
+        video: Video = youtube.get_data(row["URL"])
+        if not video:
             continue
-        video: Video = youtube.get_data(ID[0])
         df.at[index, "Title"] = video.Title
         df.at[index, "Views"] = video.Views
         df.at[index, "Likes"] = video.Likes
@@ -46,9 +47,8 @@ def main(API_KEY: str):
         df.at[index, "Comments"] = video.Comments
         df.at[index, "Like/Dislike"] = video.Likes / video.Dislikes
         df.at[index, "Likes/Views"] = video.Likes / video.Views
-    print(df[["Name", "Views", "Likes", "Dislikes", "Comments", "Like/Dislike", "Likes/Views"]])
+    print(df[["Name", "Views", "Likes", "Dislikes", "Comments", "Like/Dislike", "Likes/Views", "Title"]])
     df.to_csv("videos.csv", index=False)
-    
-if __name__ == "__main__":
-    API_KEY: str = "XXX" # put API key here (make sure there are quotes)
-    main(API_KEY)
+
+API_KEY: str = "XXX" # put API key here (make sure there are quotes)
+main(API_KEY)
